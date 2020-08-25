5ba255c6f1635a942887e5185e4b370187e41066
Eliorco <eliorc1988@gmail.com>
2020-08-25 10:19:56 +0300

    Improved the UI to look much better Added Fixed Run with expert level

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,19 +1,17 @@
 import webbrowser
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from time import sleep
 from game import Game, Board, Cube
 import json
-
-# page setup
-input_page = f''
 
 #server setup
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = "$2b$12$O6de.w1FB5HTnee8Ak9WLusDlGoouoVT5CAQGGEwTMfD6nUI/BUiC"
-cube_list = []
+cube_list = [] # TODO refactor it to a better solution user input
 
-def reset_input(cube_list):
+def reset_input():
+    global cube_list
     cube_list = []
 
 @app.route("/", methods=['GET', 'POST'])
@@ -31,12 +29,18 @@ def index():
             cube_list.append(c)
     return render_template("index.html", cells=[])
 
+@app.route("/fixed_level", methods=['GET', 'POST'])
+def fixed_level():
+    global cube_list
+    cube_list = Game.sudoku4expert
+    return redirect(url_for('start_game'))
+
 @app.route("/start", methods=['GET', 'POST'])
 def start_game():
     # create game instance
     g = Game(Board(9))
     if g.start(cube_list):
-        reset_input(cube_list)
+        reset_input()
         return render_template("index.html", solved=True, cells=g.board.grid)
 
     return render_template("index.html")
@@ -44,8 +48,9 @@ def start_game():
 
 @app.route("/reset", methods=['GET', 'POST'])
 def reset_board():
-    reset_input(cube_list)
+    reset_input()
     return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
